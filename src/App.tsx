4c36cf6e1e29e5750fbd7df0c7577b20458e430a
Mridul Khanal <khanalmridul30@gmail.com>
2026-01-30 14:49:38 +0545

    wait and watch

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+// src/App.tsx
+import './index.css'
+
+function App() {
+  return (
+    <>
+      {/* Matrix Rain Background */}
+      <div className="matrix-rain">
+        {/* We'll fill this with animated columns soon */}
+      </div>
+
+      <div className="app-container">
+        <h1>Mridul Khanal</h1>
+        <p className="tagline">HACKING THE WEB • ONE LINE AT A TIME</p>
+        <p style={{ marginTop: '2rem', fontSize: '1.2rem' }}>
+          Web Developer • Seeking Internship 2026
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
